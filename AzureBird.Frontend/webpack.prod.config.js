a4ff221faea43fb3d1c9a85c3eea5436cbee34b9
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-11-16 13:58:40 +0100

    Webpack compiles

--- AzureBird.Frontend/webpack.prod.config.js
+++ AzureBird.Frontend/webpack.prod.config.js
@@ -1,73 +1,77 @@
-var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var WebpackCleanupPlugin = require('clean-webpack-plugin');
+var CleanPlugin = require('clean-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require("webpack");
+var path = require('path');
 
 module.exports = {
-  entry: './src/js/index.jsx',
-  output: {
-    path: './public',
-    publicPath: null,		
-    filename: 'bundle.js?[hash]'
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-        {
-            test: /\.jsx?$/,
-            loaders: ['babel?presets[]=es2015,presets[]=react', 'eslint'],
-            exclude: /node_modules/
-        },
-        {
-            test: /\.scss$/,
-            loader: ExtractTextPlugin.extract('style', 'css!sass?sourceMap')
-        },
-        {
-            test: /(\.css)$/, 
-            loaders: ['style', 'css']
-        },
-        {
-            test: /\.json$/,
-            loaders: ['raw']
-        },
-        {
-            test: /\.(jpg|png|svg)$/,
-            loaders: ['file?name=[name].[ext]?[hash]']
-        },
-        {
-            test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-            loaders: ['url?name=[name].[ext]?[hash]']
-        }
-    ]
-  },
-  plugins: [
-    new WebpackCleanupPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        drop_console: true,
-        drop_debugger: true
-      }
-    }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new ExtractTextPlugin({
-      filename: 'style.css',
-      allChunks: true
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/html/index.html',
-      files: {
-        css: ['style.css'],
-        js: ['bundle.js']
-      }
-    })
-  ]
+    entry: './src/js/index.jsx',
+    output: {
+        path: './public',
+        publicPath: null,
+        filename: 'bundle.js?[hash]'
+    },
+    devtools: 'inline-source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel?presets[]=es2015,presets[]=react', 'eslint'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style', 'css!sass?sourceMap')
+            },
+            {
+                test: /(\.css)$/, 
+                loaders: ['style', 'css']
+            },
+            {
+                test: /\.json$/,
+                loaders: ['raw']
+            },
+            {
+                test: /\.(jpg|png|svg)$/,
+                loaders: ['file?name=[name].[ext]?[hash]']
+            },
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loaders: ['url?name=[name].[ext]?[hash]']
+            }
+        ]
+    },
+    plugins: [
+        new CleanPlugin(['public'], {
+            root: path.resolve(__dirname, '../')
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+              NODE_ENV: '"production"'
+            }
+          }),
+          new webpack.optimize.UglifyJsPlugin({
+            compress: {
+              warnings: false,
+              screw_ie8: true,
+              drop_console: true,
+              drop_debugger: true
+            }
+          }),
+        new HtmlWebpackPlugin({
+            template: './src/html/index.html',
+            hash: true
+        }),
+        new ExtractTextPlugin('styles.css?[contenthash]'),
+        new webpack.ProvidePlugin({
+               $: "jquery",
+               jQuery: "jquery"
+           })
+    ],
+    node: {
+        fs: "empty"
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
 };
