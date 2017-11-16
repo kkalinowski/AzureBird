160fd9984c825e5516b120143382aa829275fb85
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-11-16 11:01:51 +0100

    Reworking webpack config

--- AzureBird.Frontend/webpack.prod.config.js
+++ AzureBird.Frontend/webpack.prod.config.js
@@ -1,15 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
-// var loaders = require('./webpack.loaders');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var WebpackCleanupPlugin = require('clean-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-// loaders.push({
-//   test: /\.scss$/,
-//   loader: ExtractTextPlugin.extract({fallback: 'style-loader', use : 'css-loader?sourceMap&localIdentName=[local]___[hash:base64:5]!sass-loader?outputStyle=expanded'}),
-//   exclude: ['node_modules']
-// });
 
 module.exports = {
   entry: [
