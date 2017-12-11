d52b38cf9205c246ae42846ef2231ec442a5f540
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-12-11 09:34:22 +0100

    Displaying texts

--- AzureBird.API/AzureBird.API/AzureBird/Controllers/HomeController.cs
+++ AzureBird.API/AzureBird.API/AzureBird/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using AzureBird.Models;
+using lib12.Collections;
+using lib12.Data.Random;
 
 namespace AzureBird.Controllers
 {
@@ -8,7 +12,13 @@ namespace AzureBird.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var models = CollectionFactory.CreateEnumerable(Rand.NextInt(7, 15), x => new Content
+            {
+                Date = Rand.NextDateTime(DateTime.Now.AddDays(-7), DateTime.Now),
+                Author = Rand.NextFullName(),
+                Text = FakeData.LoremIpsumParagraph
+            }).OrderByDescending(x => x.Date);
+            return View(models);
         }
 
         public IActionResult Error()
---
+++ AzureBird.API/AzureBird.API/AzureBird/Models/Content.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureBird.Models
+{
+    public class Content
+    {
+        public string Author { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
