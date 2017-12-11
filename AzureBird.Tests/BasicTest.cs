dfaf7ac9f67b0c0d17354c6ecdf3e99496830a86
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-12-11 11:08:39 +0100

    Added test project

---
+++ AzureBird.Tests/BasicTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace AzureBird.Tests
+{
+    public class BasicTest
+    {
+        [Fact]
+        public void basic_test_is_valid()
+        {
+            Assert.True(true);
+        }
+    }
+}
